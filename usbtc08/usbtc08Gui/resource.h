f6a63ccf15810bc467429fa6e0661ab63ab1ee19
hitesh.mistry <hsmistry@users.noreply.github.com>
2017-04-11 11:52:55 +0100

    Added resource.h to project

---
+++ usbtc08/usbtc08Gui/resource.h
@@ -0,0 +1,65 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by pico_gui.rc
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDP_OLE_INIT_FAILED             100
+#define IDS_ABOUTBOX                    101
+#define IDD_PICO_GUI_DIALOG             102
+#define IDP_SOCKETS_INIT_FAILED         103
+#define IDR_MAINFRAME                   128
+#define IDC_COMBO_SAMPLEINTERVAL        1000
+#define IDC_CHECK_A                     1001
+#define IDC_CHECK_B                     1002
+#define IDC_CHECK_C                     1003
+#define IDC_CHECK_D                     1004
+#define IDC_CHECK_A_50OHM               1005
+#define IDC_CHECK_B_50OHM               1006
+#define IDC_CHECK_C_50OHM               1007
+#define IDC_CHECK_D_50OHM               1008
+#define IDC_LIST1                       1009
+#define IDC_STATUS                      1010
+#define ID_OPENPICO                     1011
+#define IDC_LOGFILENAME                 1012
+#define IDC_COMBO_RANGE_A               1013
+#define IDC_COMBO_RANGE_B               1014
+#define IDC_COMBO_RANGE_C               1015
+#define IDC_COMBO_RANGE_D               1016
+#define IDC_OFFSET_A                    1017
+#define IDC_OFFSET_B                    1018
+#define IDC_OFFSET_C                    1019
+#define IDC_OFFSET_D                    1020
+#define IDC_DESCRLINE_0                 1021
+#define ID_STREAM                       1021
+#define IDC_THRESMAX_A                  1022
+#define IDC_THRESMAX_B                  1023
+#define IDC_THRESMAX_C                  1024
+#define IDC_THRESMAX_D                  1025
+#define IDC_THRES_A                     1026
+#define IDC_THRES_B                     1027
+#define IDC_THRES_C                     1028
+#define IDC_THRES_D                     1029
+#define IDC_POLLTIMEOUT                 1030
+#define IDC_PULSERATE_B                 1031
+#define IDC_PULSERATE_C                 1032
+#define IDC_PULSERATE_D                 1033
+#define IDC_MEAN_A                      1034
+#define IDC_MEAN_B                      1035
+#define IDC_MEAN_C                      1036
+#define IDC_MEAN_D                      1037
+#define IDC_COMBO_BUFFERSIZE            1038
+#define IDC_PULSERATE_A                 1039
+#define IDC_COMBO_BUFFERSIZE2           1040
+#define IDC_COMBO_POSTTRIGGER           1040
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1018
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
