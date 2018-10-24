4c088cb38dffe387b5180b9aeb953f97e5cf2d49
hsmistry <hsmistry@users.noreply.github.com>
2018-10-24 13:56:35 +0100

    Update ps4000 files
    See also: PSDK-833

--- ps4000/ps4000Con/ps4000Con.c
+++ ps4000/ps4000Con/ps4000Con.c
@@ -1,6 +1,6 @@
 /*******************************************************************************
  *
- * Filename: ps4000con.c
+ * Filename: ps4000Con.c
  *
  * Description:
  *   This is a console mode program that demonstrates how to perform 
@@ -47,12 +47,12 @@
  *
  *		Place this file in the same folder as the files from the linux-build-files
  *		folder. In a terminal window, use the following commands to build
- *		the ps4000con application:
+ *		the ps4000Con application:
  *
  *			./autogen.sh <ENTER>
  *			make <ENTER>
  *
- * Copyright (C) 2009 - 2017 Pico Technology Ltd. See LICENSE file for terms.
+ * Copyright (C) 2009-2018 Pico Technology Ltd. See LICENSE file for terms.
  *
  ******************************************************************************/
 
