fecc35de38a005cad339077de9425d45474832bd
hsmistry <hsmistry@users.noreply.github.com>
2018-10-24 14:20:02 +0100

    Rename ps6000 example files
    See also: PSDK-833

--- ps6000/ps6000Con/ps6000Con.c
+++ ps6000/ps6000Con/ps6000Con.c
@@ -1,6 +1,6 @@
 /**************************************************************************
  *
- * Filename: ps6000con.c
+ * Filename: ps6000Con.c
  *
  * Description:
  *   This is a console mode program that demonstrates how to use the
@@ -46,12 +46,12 @@
  *
  *		Place this file in the same folder as the files from the linux-build-files
  *		folder. In a terminal window, use the following commands to build
- *		the ps6000con application:
+ *		the ps6000Con application:
  *
  *			./autogen.sh <ENTER>
  *			make <ENTER>
  *
- * Copyright (C) 2009-2017 Pico Technology Ltd. See LICENSE file for terms.
+ * Copyright (C) 2009-2018 Pico Technology Ltd. See LICENSE file for terms.
  *
  **************************************************************************/
 
